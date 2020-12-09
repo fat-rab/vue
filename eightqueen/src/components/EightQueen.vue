e60c9af819b9e752462d69f6ac54467af1d54d82
s1575478687 <1575478687@qq.com>
2020-12-09 18:01:22 +0800

    vue

---
+++ eightqueen/src/components/EightQueen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="title">八皇后问题</div>
+
+    <div class="grid">
+      <div class="row" v-for="(row, index) in grids" :key="index">
+        <div class="cell" v-for="(cell) in row" :key="cell.key">
+          <div v-if="cell.ok">Q</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const grids = new Array(8).fill(1).map((_, r) => {
+  return new Array(8).fill(1).map((_, c) => {
+    return {
+      key: `key-${r * 8 + c}`,
+      ok: false
+    };
+  });
+});
+
+export default {
+  data() {
+    return {
+      grids
+    };
+  }
+};
+</script>
